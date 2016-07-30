37ba20500f34ca5c60e731c5e5bad98358d5fb7c
Henrique Bastos <henrique@bastos.net>
2016-07-30 11:50:56 -0300

    Extract Spreadsheet class to it's own module

---
+++ spreadsheet.py
@@ -0,0 +1,25 @@
+from itertools import chain
+
+import xlrd
+from viewport import Viewport
+
+
+class Spreadsheet:
+    def __init__(self, filename):
+        book = xlrd.open_workbook(filename, formatting_info=True)
+        sheet = book.sheet_by_index(0)
+
+        self.sheet = sheet
+        self.vp = Viewport(2, 1, 12, 4)
+
+    @property
+    def headers(self):
+        return [self.sheet.cell_value(i, j) for i, j in self.vp.rows[0]]
+
+    @property
+    def data(self):
+        return [[self.sheet.cell_value(i, j) for i, j in indexes]
+                for indexes in self.vp.rows[1:]]
+
+    def __iter__(self):
+        return chain([self.headers], self.data)
