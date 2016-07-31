c866cc7e7252df6f6dc6b13368c4beaf8da7822e
Henrique Bastos <henrique@bastos.net>
2016-07-31 13:43:23 -0300

    Refactor CardStatement.__str__

--- statement.py
+++ statement.py
@@ -6,7 +6,4 @@ class CardStatement(Spreadsheet):
     viewport = Viewport(2, 1, 12, 4)
 
     def __str__(self):
-        out = []
-        for row in self:
-            out.append('{3: >6} {2}'.format(*row))
-        return '\n'.join(out)
+        return '\n'.join('{3: >6} {2}'.format(*row) for row in self)
