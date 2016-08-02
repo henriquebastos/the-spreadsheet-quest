3a7961e537be4030257180cd1e0f0220e37cb0b2
Henrique Bastos <henrique@bastos.net>
2016-08-02 14:38:43 -0300

    Make Viewport optional on instantiating Spreadsheet

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,15 +1,20 @@
 from itertools import chain
 
 import xlrd
+from viewport import Viewport
 
 
 class Spreadsheet:
-    def __init__(self, sheet, viewport):
+    viewport = Viewport(0, 0, 1, 1)
+
+    def __init__(self, sheet, viewport=None):
         self.sheet = sheet
-        self.vp = viewport
+
+        if viewport:
+            self.viewport = viewport
 
     @classmethod
-    def from_xls(cls, filename, viewport):
+    def from_xls(cls, filename, viewport=None):
         book = xlrd.open_workbook(filename, formatting_info=True)
         sheet = book.sheet_by_index(0)
 
@@ -17,12 +22,12 @@ class Spreadsheet:
 
     @property
     def headers(self):
-        return [self.sheet.cell_value(i, j) for i, j in self.vp.rows[0]]
+        return [self.sheet.cell_value(i, j) for i, j in self.viewport.rows[0]]
 
     @property
     def data(self):
         return [[self.sheet.cell_value(i, j) for i, j in indexes]
-                for indexes in self.vp.rows[1:]]
+                for indexes in self.viewport.rows[1:]]
 
     def __iter__(self):
         return chain([self.headers], self.data)
